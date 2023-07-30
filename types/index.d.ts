af703ab37248dcb0ca936a64e0404cdc3694a9fe
Mr. Hands <mrhands31@gmail.com>
2023-07-30 10:27:04 +0200

    Add function declarations.

---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+declare global {
+	// Output function only when in development mode
+	function $devOnly(what: () => void): void;
+
+	// Output function only when in production mode
+	function $prodOnly(what: () => void): void;
+
+	// Log a message with TRACE priority
+	function $logTrace(domain: object | string, message: string): void;
+
+	// Log a message with INFO priority
+	function $logInfo(domain: object | string, message: string): void;
+
+	// Log a message with WARN priority
+	function $logWarn(domain: object | string, message: string): void;
+
+	// Log a message with ERROR priority
+	function $logError(domain: object | string, message: string): void;
+
+	// Log a message with FATAL priority
+	function $logFatal(domain: object | string, message: string): void;
+}
+export {};
