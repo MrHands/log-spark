7a5f8f8472dfd18dcbee934879d90037a0e20cec
Mr. Hands <mrhands31@gmail.com>
2023-07-30 16:48:22 +0200

    Fix not being able to use defined types in tests.

--- tests/log-info/log-info.test.ts
+++ tests/log-info/log-info.test.ts
@@ -1,16 +1,12 @@
 import { expect } from 'chai';
 
-declare global {
-	function $logInfo(domain: object | string, message: string): void;
-}
-
 function CaptureMacro(run: () => void) {
 	let result = '';
 	const original = console.log;
 	console.log = (value: string) => result += value;
 
 	run();
-	
+
 	console.log = original;
 
 	return result;
