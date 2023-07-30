451111ab36cbc1f465b178157dae1296aca5ca0d
Mr. Hands <mrhands31@gmail.com>
2023-07-30 10:42:26 +0200

    Attempt to build tests with Webpack.

---
+++ tests/.eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention -- Can't enable on this file */
+
+module.exports = {
+	extends: ['../.eslintrc.js'],
+	rules: {
+		'import/no-extraneous-dependencies': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+	},
+};
+
+/* eslint-enable @typescript-eslint/naming-convention */
---
+++ tests/webpack.config.ts
@@ -0,0 +1,69 @@
+import path from 'path';
+
+import {
+	DevOnlyTransform,
+	LoggingTransform,
+	ProdOnlyTransform,
+} from '../source';
+import tsconfig from '../tsconfig.json';
+
+export default (env: NodeJS.ProcessEnv) => {
+	const targetPath = path.resolve(__dirname, '../build/tests');
+
+	const nodeEnv =		(env.production != null ? 'production' : null)
+		?? env.NODE_ENV
+		?? 'development';
+
+	const isProduction = nodeEnv === 'production';
+	const isDevelopment = !isProduction;
+
+	const compilerOptions = Object.assign(tsconfig.compilerOptions, {
+		// outDir: path.resolve(targetPath, 'js'),
+		outDir: targetPath,
+	});
+
+	return {
+		entry: {
+			'log-info': 'log-info/log-info.test.ts',
+		},
+
+		module: {
+			rules: [
+				// compile typescript
+				{
+					test: /\.ts$/u,
+					// include: [path.resolve(__dirname, 'js')],
+					use: [
+						{
+							loader: require.resolve('ts-loader'),
+							options: {
+								compilerOptions,
+								// happyPackMode: true,
+								getCustomTransformers: () => ({
+									before: [
+										DevOnlyTransform(isDevelopment),
+										ProdOnlyTransform(isProduction),
+										LoggingTransform,
+									],
+								}),
+							},
+						},
+					],
+				},
+			],
+		},
+
+		output: {
+			chunkFormat: 'commonjs',
+			chunkLoading: 'require',
+			environment: {
+				module: false,
+			},
+			path: targetPath,
+			filename: 'js/[name].bundle.js',
+			sourceMapFilename: '[file].map',
+		},
+
+		target: 'es2020',
+	};
+};
