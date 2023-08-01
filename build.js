f99dc354dbe2cc3483761dac5ba0b8274f02b2ba
Mr. Hands <mrhands31@gmail.com>
2023-08-01 10:34:50 +0200

    Add test flag to build script.

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 (async () => {
 	const { spawn } = require('child_process');
-	const commander = require('commander');
+	const { program } = require('commander');
 	const fs = require('fs');
 	const path = require('path');
 	const process = require('process');
@@ -9,10 +9,12 @@
 	const readdirPromise = util.promisify(fs.readdir);
 	const statPromise = util.promisify(fs.stat);
 
-	commander
+	program
 		.version('1.0.0')
-		.option('-t, --tests', 'run tests')
+		.option('-t, --test', 'run tests')
 		.parse(process.argv);
+
+	const options = program.opts();
 
 	function RunCommand(...command) {
 		let p = spawn(command[0], command.slice(1), { shell: true });
@@ -21,7 +23,7 @@
 				console.log(result.toString());
 			});
 			p.stderr.on('data', (result) => {
-				console.error(`Failed to run "${command.join(' ')}": "${result}"`);
+				console.error(result.toString());
 			});
 			p.on('close', (code) => {
 				if (code !== 0) {
@@ -36,7 +38,7 @@
 	const root = process.cwd();
 
 	try {
-		// library
+		// compile library
 
 		console.log('Compiling library...');
 
@@ -44,41 +46,53 @@
 			throw new Error('Failed to compile.');
 		}
 
-		// unit tests
+		if (options.test) {
+			// compile unit tests
 
-		process.chdir(path.resolve(root, 'tests'));
+			process.chdir(path.resolve(root, 'tests'));
 
-		console.log('Compiling unit tests...');
-
-		if (await RunCommand('npx', 'tsc') !== 0) {
-			throw new Error('Failed to compile.');
-		}
-
-		// integration tests
-
-		console.log('Compiling integration tests...');
-
-		let fileList = await readdirPromise(process.cwd(), null);
-
-		const result = await Promise.all(fileList.map(async (file) => {
-			const integrationDir = path.resolve(process.cwd(), file);
-			const stat = await statPromise(integrationDir);
-			
-			if (stat && stat.isDirectory()) {
-				return integrationDir;
-			}
-		}));
-		const dirs = result.filter((it) => typeof it !== 'undefined');
-
-		dirs.forEach(async (integrationDir) => {
-			process.chdir(integrationDir);
-
-			console.log(`  - ${path.basename(integrationDir)}`);
+			console.log('Compiling unit tests...');
 
 			if (await RunCommand('npx', 'tsc') !== 0) {
 				throw new Error('Failed to compile.');
 			}
-		});
+
+			// compile integration tests
+
+			console.log('Compiling integration tests...');
+
+			let fileList = await readdirPromise(process.cwd(), null);
+
+			const result = await Promise.all(fileList.map(async (file) => {
+				const integrationDir = path.resolve(process.cwd(), file);
+				const stat = await statPromise(integrationDir);
+				
+				if (stat && stat.isDirectory()) {
+					return integrationDir;
+				}
+			}));
+			const dirs = result.filter((it) => typeof it !== 'undefined');
+
+			dirs.forEach(async (integrationDir) => {
+				process.chdir(integrationDir);
+
+				console.log(`  - ${path.basename(integrationDir)}`);
+
+				if (await RunCommand('npx', 'tsc') !== 0) {
+					throw new Error('Failed to compile.');
+				}
+			});
+
+			// run tests
+
+			console.log('Running all tests...');
+
+			process.chdir(path.resolve(root, 'tests'));
+
+			if (await RunCommand('jest', '--verbose') !== 0) {
+				throw new Error('Tests failed.');
+			}
+		}
 
 		console.log('');
 		console.log('DONE');
